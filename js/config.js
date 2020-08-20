75be4c36922ebe0e0227a340ab6aa172b91020be
Deepak-tech8 <66052819+Deepak-tech8@users.noreply.github.com>
2020-08-20 14:02:03 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_LR95YozQ5', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '39m1u278u3vhgtgjurnr71au64', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
