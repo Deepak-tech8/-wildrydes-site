a80600d5a7fe385828a98a573bf2cfd7e96fcf22
Deepak-tech8 <66052819+Deepak-tech8@users.noreply.github.com>
2020-08-20 14:05:34 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'ap-south-1_LR95YozQ5', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '39m1u278u3vhgtgjurnr71au64', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
